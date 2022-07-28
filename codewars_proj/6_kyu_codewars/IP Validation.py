232966e1a4053e16242639dd168a07dee7c0a7cf
BorgonPL <kasprzyk.szymon.99@gmail.com>
2022-07-29 01:51:36 +0200

    Create IP Validation

--- codewars_proj/6_kyu_codewars/IP Validation.py
+++ codewars_proj/6_kyu_codewars/IP Validation.py
@@ -2,7 +2,7 @@ def is_valid_IP(string):
     s=string.split('.')
     if len(s)==4:
         for x in s:
-            if str(x).isdigit()!=True or int(x)<0 or int(x)>255 or str(int(x))==x:
+            if str(x).isdigit()!=True or int(x)<0 or int(x)>255 or str(int(x))!=x:
                 return False
     else: 
         return False
